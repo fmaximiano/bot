bd6a92d50a5bae769add15c89568c89f96761f2c
Frederico Maximiano <f.a.maximiano@gmail.com>
2024-08-22 21:19:04 -0300

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ app = Flask(__name__)
 client = AzureOpenAI(
     azure_endpoint = os.getenv("AZURE_OPENAI_ENDPOINT"),
     api_key= os.getenv("AZURE_OPENAI_API_KEY"),
-    api_version="2024-06-01 -preview",
+    api_version="2024-05-01-preview",
 )
 
 assistant = os.getenv("ASSISTANT_ID")
